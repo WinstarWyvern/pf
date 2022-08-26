2588ea2a0c9f9b0e9013143d3b3fa231edd26d71
MikazukiMizuki <winstonak13112001@gmail.com>
2022-08-26 15:01:26 +0700

    - Add wallpaper - Add component css

--- src/components/Biodata/Biodata.jsx
+++ src/components/Biodata/Biodata.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import "../components.css"
+// import "./Biodata.css"
 
 const Biodata = () => {
   return (
-    <div>Biodata</div>
+    <div className="custom-bg container-fluid h-708">
+      <div>Biodata</div>
+    </div>
   )
 }
 
