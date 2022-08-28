4306a9aac599c95f401acafa8e2a716d9bf85d86
MikazukiMizuki <winstonak13112001@gmail.com>
2022-08-28 20:12:48 +0700

    - Add bootstrap icon - Add about me content - Add image slot content - Add css component

--- src/components/Biodata/Biodata.jsx
+++ src/components/Biodata/Biodata.jsx
@@ -1,11 +1,163 @@
 import React from 'react'
 import "../components.css"
-// import "./Biodata.css"
+import { BsLine, BsGithub, BsTwitter, BsDiscord } from "react-icons/bs";
+
 
 const Biodata = () => {
   return (
-    <div className="custom-bg container-fluid h-708">
-      <div>Biodata</div>
+    <div className="custom-bg-1 h-708 custom-container">
+      <div className='border-black-10 w-85 h-85 transparent-bg-1 text-bold text-black p-5 fs-20'>
+        <div class="d-flex w-100">
+          <div className='w-75'>
+            <div class="row my-1">
+
+              <div class="row">
+                <div class="col-3 fs-25">
+                  Profile
+                </div>
+                <div class="col-1">
+                  &nbsp;
+                </div>
+                <div class="col-5">
+                  &nbsp;
+                </div>
+              </div>
+
+              <div class="col-3">
+                Full Name
+              </div>
+              <div class="col-1">
+                :
+              </div>
+              <div class="col-5">
+                Winston Adinata Kusumo
+              </div>
+            </div>
+
+            <div class="row my-1">
+              <div class="col-3">
+                Nickname
+              </div>
+              <div class="col-1">
+                :
+              </div>
+              <div class="col-5">
+                Winston, Winsta
+              </div>
+            </div>
+
+            <div class="row my-1">
+              <div class="col-3">
+                Date of Birth
+              </div>
+              <div class="col-1">
+                :
+              </div>
+              <div class="col-5">
+                November 13th 2001
+              </div>
+            </div>
+
+            <div class="row my-1">
+              <div class="col-3">
+                Gender
+              </div>
+              <div class="col-1">
+                :
+              </div>
+              <div class="col-5">
+                Male
+              </div>
+            </div>
+
+            <div class="row my-1">
+              <div class="col-3">
+                Email
+              </div>
+              <div class="col-1">
+                :
+              </div>
+              <div class="col-5">
+                winstonadinata@gmail.com
+              </div>
+            </div>
+
+            <div class="row my-1">
+              <div class="col-3">
+                Phone
+              </div>
+              <div class="col-1">
+                :
+              </div>
+              <div class="col-5">
+                +6289662358056
+              </div>
+            </div>
+
+            <div class="row mt-5">
+              <div class="col-3 fs-25">
+                Contact
+              </div>
+              <div class="col-1">
+                &nbsp;
+              </div>
+              <div class="col-5">
+                &nbsp;
+              </div>
+            </div>
+
+            <div class="row my-1">
+              <div class="col-3">
+                Line <BsLine/>
+              </div>
+              <div class="col-1">
+                :
+              </div>
+              <div class="col-5">
+                winston.ak
+              </div>
+            </div>
+
+            <div class="row my-1">
+              <div class="col-3">
+                Twitter <BsTwitter />
+              </div>
+              <div class="col-1">
+                :
+              </div>
+              <div class="col-5">
+                @Winsta16
+              </div>
+            </div>
+
+            <div class="row my-1">
+              <div class="col-3">
+                Github <BsGithub />
+              </div>
+              <div class="col-1">
+                :
+              </div>
+              <div class="col-5">
+                MikazukiMizuki
+              </div>
+            </div>
+
+            <div class="row my-1">
+              <div class="col-3">
+                Discord <BsDiscord />
+              </div>
+              <div class="col-1">
+                :
+              </div>
+              <div class="col-5">
+                Winsta#2924
+              </div>
+            </div>
+
+          </div>
+          <div className='w-25'>image</div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,13 +4,10 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-collapse">
         <div className="navbar-nav mx-1 h5">
           <Link to="/home" className="nav-item nav-link disabled">Portofolio</Link>
-          <Link to="/biodata" className="nav-item nav-link text-white">Biodata</Link>
+          <Link to="/biodata" className="nav-item nav-link text-white">About Me</Link>
           <Link to="/skills" className="nav-item nav-link text-white">Skills</Link>
           <Link to="/pastworks" className="nav-item nav-link text-white">Past Works</Link>
           <Link to="/education" className="nav-item nav-link text-white">Education</Link>
