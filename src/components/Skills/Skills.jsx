a3aafbd5b20d4d50120b8bd737e04c742ee84e13
MikazukiMizuki <winstonak13112001@gmail.com>
2022-09-01 00:08:26 +0700

    - adjust skill container

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -7,9 +7,9 @@ import "./Skills.css"
 const Skills = () => {
   return (
     <div className="skills-bg custom-bg-config h-708 custom-container">
-      <div className='border-red-10 w-85 h-85 skills-container-bg text-bold text-black p-5 fs-20 opacity-087'>
+      <div className='border-red-10 w-85 h-85 skills-container-bg text-bold text-white p-5 fs-20 opacity-087'>
         <div class="d-flex w-100">
-
+          
         </div>
       </div>
     </div>
