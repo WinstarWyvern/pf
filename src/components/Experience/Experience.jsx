7c6983b1a23dc061e17248f1acbddf6c72443b23
winstarwyvern <winstonadinata@gmail.com>
2025-05-15 16:12:17 +0700

    Update experience and pastworks 1 year

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,11 +3,12 @@ import "./Experience.css"
 import "../components.css"
 import sokratesLogo from "../../asset/image/sokrates-logo.png"
 import blibliLogo from "../../asset/image/blibli-logo.png"
+import ambersofLogo from "../../asset/image/ambersof-logo.png"
 
 const Experience = () => {
   return (
     <div className="experience-bg custom-bg-config h-708 custom-container">
-      <div className='border-pink-10 w-75 h-80 experience-container-bg text-bold text-black p-5 fs-20 opacity-087'>
+      <div className='border-pink-10 w-75 h-80 experience-container-bg text-bold text-black p-5 fs-20 opacity-1 m-auto'>
         <div className="col-3 fs-25">
           Timeline
         </div>
@@ -30,6 +31,16 @@ const Experience = () => {
               <div className="card-body text-center">
                 <h5 className="card-title">Software Engineer Intern (Fullstack)</h5>
                 <p className="card-text">2023 - 2024</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-25 h-50 mx-3">
+            <div className="card border border-primary border-5">
+              <img className="card-img-top" src={ambersofLogo} alt="error" height={200} />
+              <div className="card-body text-center">
+                <h5 className="card-title">FrontEnd Developer (Contract)</h5>
+                <p className="card-text">2024 - 2025</p>
               </div>
             </div>
           </div>
--- src/components/PastWorks/PastWorks.jsx
+++ src/components/PastWorks/PastWorks.jsx
@@ -1,134 +1,215 @@
-import React from 'react'
-import "../components.css"
-import "./PastWorks.css"
+import React from "react";
+import "../components.css";
+import "./PastWorks.css";
 
 const PastWorks = () => {
   return (
     <div className="pastworks-bg custom-bg-config custom-container">
-      <div className='border-purple-10 w-75 h-85 bg-white text-bold text-black p-5 fs-20 opacity-087 my-3'>
+      <div className="border-purple-10 w-75 h-85 bg-white text-bold text-black p-5 fs-20 opacity-087 my-3">
         <div className="d-flex w-100 p-3 flex-column">
-
           <div>
             <h2>Personal Website Project</h2>
 
             <ul>
               <li>
-                <a href="https://winstarwyvern.github.io/Doto2/" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <a
+                  href="https://winstarwyvern.github.io/Doto2/"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer" F
+                >
                   Doto 2
                 </a>
               </li>
 
               <li>
-                <a href="https://github.com/WinstarWyvern/UMDB" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <a
+                  href="https://github.com/WinstarWyvern/UMDB"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
                   UMDB
                 </a>
               </li>
 
               <li>
-                <a href="https://github.com/WinstarWyvern/Giant-Book-Supplier" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <a
+                  href="https://github.com/WinstarWyvern/Giant-Book-Supplier"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
                   Giant Book Supplier
                 </a>
               </li>
 
               <li>
-                <a href="https://github.com/WinstarWyvern/Barbatos-Shop" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <a
+                  href="https://github.com/WinstarWyvern/Barbatos-Shop"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
                   Barbatos Shop
                 </a>
               </li>
             </ul>
-
           </div>
 
           <div>
-
             <h2>UML Project</h2>
 
             <ul>
               <li>
-                <a href="https://padlet.com/winstonkusumo/1o0oliozrfehkr08" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <a
+                  href="https://padlet.com/winstonkusumo/1o0oliozrfehkr08"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
                   KeMana Saja
                 </a>
               </li>
             </ul>
-
           </div>
 
           <div>
-
             <h2>UI/UX Project</h2>
 
             <ul>
               <li>
-                <a href="https://www.figma.com/file/0hK7yqCr6rnwkTFf47LdvV/Hotelify?node-id=750%3A617" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <a
+                  href="https://www.figma.com/file/0hK7yqCr6rnwkTFf47LdvV/Hotelify?node-id=750%3A617"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
                   Hotelify
                 </a>
               </li>
 
               <li>
-                <a href="https://www.figma.com/file/YQs2biDVOhBtuQfewdKobk/UTS-Mulmed?node-id=0%3A1" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <a
+                  href="https://www.figma.com/file/YQs2biDVOhBtuQfewdKobk/UTS-Mulmed?node-id=0%3A1"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
                   MedStore
                 </a>
               </li>
 
               <li>
-                <a href="https://www.figma.com/file/SXWOW6Z1CHdS88P90eZAMq/UI-UAS-MULMED?node-id=0%3A1" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <a
+                  href="https://www.figma.com/file/SXWOW6Z1CHdS88P90eZAMq/UI-UAS-MULMED?node-id=0%3A1"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
                   National Heroes of Indonesia Simulator
                 </a>
               </li>
 
               <li>
-                <a href="https://www.figma.com/file/SXWOW6Z1CHdS88P90eZAMq/UI-UAS-MULMED?node-id=0%3A1" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <a
+                  href="https://www.figma.com/file/SXWOW6Z1CHdS88P90eZAMq/UI-UAS-MULMED?node-id=0%3A1"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
                   AR Map
                 </a>
               </li>
 
               <li>
-                <a href="https://www.figma.com/file/5fyNUfWCUp7j55H33jKjur/Project-HCI-LE-01?node-id=0%3A1" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <a
+                  href="https://www.figma.com/file/5fyNUfWCUp7j55H33jKjur/Project-HCI-LE-01?node-id=0%3A1"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
                   Trash Collector
                 </a>
               </li>
-
             </ul>
-
           </div>
 
           <div>
-
             <h2>Thesis Project</h2>
 
             <ul>
               <li>
-                <a href="/" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <span
+                  class="text-dark text-decoration-none"
+                >
                   Member Zone Feature
-                </a>
+                </span>
               </li>
             </ul>
-
           </div>
 
           <div>
-
             <h2>Personal Mobile Android Project</h2>
 
             <ul>
               <li>
-                <a href="https://github.com/WinstarWyvern/AnimeDB" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <a
+                  href="https://github.com/WinstarWyvern/AnimeDB"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
                   AnimeDB
                 </a>
               </li>
               <li>
-                <a href="https://github.com/WinstarWyvern/CinemaCGP" class="text-dark text-decoration-none" target={'_blank'} rel="noreferrer">
+                <a
+                  href="https://github.com/WinstarWyvern/CinemaCGP"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
                   CinemaCGP
                 </a>
               </li>
             </ul>
-
           </div>
 
+          <div>
+            <h2>PT Amber Solusi Internasional</h2>
+
+            <ul>
+              <li>
+                <a
+                  href="https://lotusgarden.app/"
+                  class="text-dark text-decoration-none"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
+                  NWL (Natural Way of Living)
+                </a>
+              </li>
+              <li>
+                <span
+                  class="text-dark text-decoration-none"
+                >
+                  IFS (Integration Financial System) Telkomsel
+                </span>
+              </li>
+              <li>
+                <span
+                  class="text-dark text-decoration-none"
+                >
+                  FCNZ (Fire Control New Zealand)
+                </span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PastWorks
+export default PastWorks;
