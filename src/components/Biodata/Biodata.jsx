180211bdd7fe4b93bcc5b1ed0a23a5ff291bd0c1
Winsta <winstonadinata@gmail.com>
2026-07-04 15:07:28 +0700

    Update background

--- src/components/Biodata/Biodata.jsx
+++ src/components/Biodata/Biodata.jsx
@@ -6,7 +6,7 @@ import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 const Biodata = () => {
   return (
-    <div className="custom-bg-config h-708 custom-container border-red-10">
+    <div className="biodata-bg custom-bg-config h-708 custom-container border-red-10">
       <div className='border-black-10 w-85 h-85 biodata-container-bg text-bold text-black p-5 fs-20 opacity-087'>
         <div className="d-flex w-100">
           <div className='w-65'>
